ebcbdb52a6dd007ff175c0392635d8da2cba2329
Vinícius :3 <100241274+ViniciusTheCoder@users.noreply.github.com>
2024-09-02 23:49:11 -0300

    feat: adding logs, fixing bugs as failed autocomplete and adjusting back button function

--- main.py
+++ main.py
@@ -4,170 +4,186 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 import pandas as pd
 import time
+import logging
+
+# Configurar logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Configurações do WebDriver
 chrome_options = Options()
-chrome_options.add_argument("--headless")  # Opcional: executa o navegador em segundo plano
-service = Service(r'')  # Corrigido
-
-driver = webdriver.Chrome(service=service, options=chrome_options)
+chrome_options.add_argument("--headless")  
+service = Service(r'')  
+def setup_driver():
+    return webdriver.Chrome(service=service, options=chrome_options)
 
 def type_with_mask(input_element, text):
-    """Simula a digitação de texto em um campo com máscara."""
     for char in text:
         input_element.send_keys(char)
-        time.sleep(0.1)  # Atraso para simular a digitação
+        time.sleep(0.1)
     
-def main(sample):
+    # Verificar se todos os caracteres foram digitados
+    if input_element.get_attribute('value') != text:
+        input_element.clear()
+        input_element.send_keys(text)
+
+def login(driver, email, password):
+    driver.get('https://app.lotusmais.com.br/login')
+    
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'email'))).send_keys('your email')
+    driver.find_element(By.ID, 'password').send_keys('your password')
+    
+    login_button = driver.find_element(By.CLASS_NAME, 'btn.btn-primary')
+    login_button.click()
+    
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, 'vi')))
+    
+    codigo = input("Digite o código de autenticação no terminal: ")
+    
+    auth_input = driver.find_element(By.CLASS_NAME, 'vi')
+    auth_input.clear()
+    auth_input.send_keys(codigo)
+    
+    checkbox = driver.find_element(By.ID, 'flexCheckDefault')
+    if not checkbox.is_selected():
+        checkbox.click()
+    
     try:
-        # Acesse a URL de login
-        driver.get('https://app.lotusmais.com.br/login')
-
-        # Preencha o formulário de login
-        WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.ID, 'email'))
-        ).send_keys('#aqui você colocará seu e-mail para login')
-
-        driver.find_element(By.ID, 'password').send_keys('#aqui você colocará sua senha')
-
-        # Clique no botão de login
-        login_button = driver.find_element(By.CLASS_NAME, 'btn.btn-primary')
-        login_button.click()
-
-        # Aguarde o campo de autenticação aparecer e solicite o código
-        WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.CLASS_NAME, 'vi'))
+        confirm_button = WebDriverWait(driver, 5).until(
+            EC.element_to_be_clickable((By.CLASS_NAME, 'btn.btn-primary.w-100.mt-2'))
         )
+        driver.execute_script("arguments[0].classList.remove('disabled');", confirm_button)
+        confirm_button.click()
+    except Exception as e:
+        logging.error(f"Erro ao confirmar login: {e}")
+        raise
+
+    WebDriverWait(driver, 5).until(EC.url_to_be('https://app.lotusmais.com.br/dashboard'))
+
+def navigate_to_new_proposal(driver):
+    try:
+        nova_proposta = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.XPATH, "//span[contains(text(),'Nova Proposta')]"))
+        )
+        nova_proposta.click()
+    except Exception as e:
+        logging.error(f"Erro ao navegar para Nova Proposta: {e}")
+        raise
+
+def process_cpf(driver, cpf, index, total):
+    try:
+        input_element = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'form-control.wizard__step__input'))
+        )
+        input_element.clear()
+        type_with_mask(input_element, cpf)
         
-        # Solicite o código de autenticação
-        codigo = input("Digite o código de autenticação no terminal: ")
-
-        # Preencha o campo de autenticação com o código fornecido
-        auth_input = driver.find_element(By.CLASS_NAME, 'vi')
-        auth_input.clear()
-        auth_input.send_keys(codigo)
-
-        # Verifique e marque a checkbox se não estiver marcada
-        checkbox = driver.find_element(By.ID, 'flexCheckDefault')
-        if not checkbox.is_selected():
-            checkbox.click()
-
-        # Verifique se o botão de confirmação está habilitado e visível
-        try:
-            confirm_button = WebDriverWait(driver, 20).until(
-                EC.element_to_be_clickable((By.CLASS_NAME, 'btn.btn-primary.w-100.mt-2'))
-            )
-            # Remova a classe 'disabled' do botão para permitir o clique, se necessário
-            driver.execute_script("arguments[0].classList.remove('disabled');", confirm_button)
-            confirm_button.click()
-        except Exception as e:
-            print(f"Erro ao encontrar ou clicar no botão de confirmação. Erro: {e}")
-            driver.quit()
-            return
-
-        # Aguarde redirecionamento para a página principal
-        WebDriverWait(driver, 20).until(
-            EC.url_to_be('https://app.lotusmais.com.br/dashboard')
+        busca_button = WebDriverWait(driver, 1).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'btn.btn-primary.d-flex.align-items-center.justify-content-center'))
         )
+        time.sleep(1)
+        driver.execute_script("arguments[0].classList.remove('disabled');", busca_button)
+        
+        if busca_button.is_displayed() and busca_button.is_enabled():
+            busca_button.click()
+        else:
+            logging.warning("O botão de busca está desabilitado.")
+            return None
 
-        # Continue com o fluxo original
+        time.sleep(1)
+
+        saldo = "0"
+        found_total_transfer = False
         try:
-            nova_proposta = WebDriverWait(driver, 20).until(
-                EC.presence_of_element_located((By.XPATH, "//span[contains(text(),'Nova Proposta')]"))
+            total_transfer_element = WebDriverWait(driver, 5).until(
+                EC.presence_of_element_located((By.CLASS_NAME, 'totalTransfer'))
             )
-            nova_proposta.click()
-        except Exception as e:
-            print(f"Erro ao encontrar ou clicar no elemento 'Nova Proposta'. Erro: {e}")
-            driver.quit()
-            return
+            saldo = total_transfer_element.find_element(By.TAG_NAME, 'h3').text
+            found_total_transfer = True
+        except (TimeoutException, NoSuchElementException):
+            logging.info(f"Saldo não encontrado para CPF {cpf}. Definindo como 0.")
 
-        # Acesse a planilha pública
-        url_planilha = 'C:\\Users\\admin\\Documents\\Projects\\automation\\planilha.xlsx'
-        df = pd.read_excel(url_planilha, engine='openpyxl')
+        if found_total_transfer:
+            
+            try:
+                
+                dashboard_button = WebDriverWait(driver, 5).until(
+                    EC.element_to_be_clickable((By.XPATH, "//span[contains(text(),'Dashboard')]"))
+                )
+                dashboard_button.click()
+                logging.info("Clicou em 'Dashboard'")
 
-        # Verifique se há CPFs na planilha
+                
+                time.sleep(2)
+
+                
+                nova_proposta = WebDriverWait(driver, 5).until(
+                    EC.element_to_be_clickable((By.XPATH, "//span[contains(text(),'Nova Proposta')]"))
+                )
+                nova_proposta.click()
+                logging.info("Clicou em 'Nova Proposta'")
+
+            except Exception as e:
+                logging.error(f"Erro ao navegar para Nova Proposta após encontrar saldo: {e}")
+        else:
+            
+            try:
+                back_button = WebDriverWait(driver, 10).until(
+                    EC.element_to_be_clickable((By.CSS_SELECTOR, 'button.btn.btn-outline-primary.d-flex.align-items-center.justify-content-center'))
+                )
+                driver.execute_script("arguments[0].click();", back_button)
+                logging.info("Clicou no botão 'Voltar'")
+            except Exception as e:
+                logging.error(f"Erro ao clicar no botão 'Voltar': {e}")
+
+       
+        logging.info(f"Processados {index + 1} de {total} CPFs.")
+
+        return saldo
+
+    except Exception as e:
+        logging.error(f"Erro ao processar CPF {cpf}: {e}")
+        return None
+
+def main(sample):
+    driver = setup_driver()
+    try:
+        login(driver, 'seu_email@exemplo.com', 'sua_senha')
+        navigate_to_new_proposal(driver)
+
+        df = pd.read_excel('\\planilha.xlsx', engine='openpyxl')
+        
         if df.empty or 'CPF' not in df.columns:
-            print("A planilha não contém a coluna 'CPF'.")
-            driver.quit()
+            logging.error("A planilha não contém a coluna 'CPF'.")
             return
 
-        # Certifique-se de que a coluna 'CPF' é tratada como string
         df['CPF'] = df['CPF'].astype(str)
+        df['Saldo'] = ""  
 
-        # Itera sobre os CPFs, limitado pelo parâmetro sample
+        total_cpfs = len(df.head(sample)) 
+
         for index, row in df.head(sample).iterrows():
             cpf = row['CPF']
-            print(f"Processando CPF: {cpf}")
+            logging.info(f"Processando CPF: {cpf}")
+            
+            saldo = process_cpf(driver, cpf, index, total_cpfs)
+            if saldo is not None:
+                df.at[index, 'Saldo'] = saldo
+                logging.info(f"Saldo do CPF {cpf}: {saldo}")
+            else:
+                df.at[index, 'Saldo'] = "0"
+                logging.warning(f"Definindo saldo como 0 para CPF {cpf} devido a erro no processamento.")
 
-            try:
-                # Preencha o input com o CPF usando digitação simulada
-                input_element = WebDriverWait(driver, 5).until(
-                    EC.presence_of_element_located((By.CLASS_NAME, 'form-control.wizard__step__input'))
-                )
-                
-                # Limpar o campo
-                input_element.clear()
-
-                # Simular a digitação do CPF
-                type_with_mask(input_element, cpf)
-                
-                # Clique no botão para buscar
-                busca_button = WebDriverWait(driver, 5).until(
-                    EC.presence_of_element_located((By.CLASS_NAME, 'btn.btn-primary.d-flex.align-items-center.justify-content-center'))
-                )
-
-                # Adicione um atraso para garantir que o botão se torne clicável
-                time.sleep(5)
-                
-                # Remover a classe 'disabled' do botão
-                driver.execute_script("arguments[0].classList.remove('disabled');", busca_button)
-                
-                if busca_button.is_displayed() and busca_button.is_enabled():
-                    busca_button.click()
-                else:
-                    print("O botão de busca ainda está desabilitado.")
-                    continue
-
-                # Aguarde 10 segundos para o site carregar
-                time.sleep(10)
-
-                # Verifique se a classe 'totalTransfer' existe
-                try:
-                    total_transfer_element = WebDriverWait(driver, 5).until(
-                        EC.presence_of_element_located((By.CLASS_NAME, 'totalTransfer'))
-                    )
-                    saldo = total_transfer_element.find_element(By.TAG_NAME, 'span').text
-
-                    # Atualize a planilha com o saldo
-                    df.at[index, 'Saldo'] = saldo
-                    print(f"Saldo do CPF {cpf}: {saldo}")
-
-                    # Clique no botão para seguir para o próximo CPF
-                    next_button = WebDriverWait(driver, 5).until(
-                        EC.element_to_be_clickable((By.CLASS_NAME, 'btn.btn-outline-primary.d-flex.align-items-center.justify-content-center'))
-                    )
-                    next_button.click()
-
-                except Exception as e:
-                    print(f"Saldo não encontrado para CPF {cpf}. Erro: {e}")
-                    # Clique no botão para seguir para o próximo CPF
-                    next_button = WebDriverWait(driver, 5).until(
-                        EC.element_to_be_clickable((By.CLASS_NAME, 'btn.btn-outline-primary.d-flex.align-items-center.justify-content-center'))
-                    )
-                    next_button.click()
-                
-            except Exception as e:
-                print(f"Erro ao processar CPF {cpf}. Erro: {e}")
-
-        # Salve a planilha atualizada
         df.to_excel('planilha_atualizada.xlsx', index=False)
+        logging.info("Processamento concluído. Planilha atualizada salva.")
 
+    except Exception as e:
+        logging.error(f"Erro durante a execução: {e}")
     finally:
-        # Feche o navegador
         driver.quit()
 
 if __name__ == "__main__":
-    sample = 5  # Defina o número de CPFs a serem processados
+    sample = 11911
     main(sample)
